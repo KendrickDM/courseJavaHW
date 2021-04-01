7cda8fce99d6a403773f8665a35f7f49ce73abae
KendrickDM <youngman0220@mail.ru>
2021-04-02 00:39:59 +0300

    Modified HW_1

---
+++ src/com/courseJava/PiizaMain.java
@@ -0,0 +1,17 @@
+package com.courseJava;
+
+import java.util.Scanner;
+import static com.courseJava.Pizza.eatPizza;
+
+public class PiizaMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("You want pizza?! Enter:(Yes/No)");
+
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.nextLine();
+
+        System.out.println(eatPizza(answer));
+    }
+}
---
+++ src/com/courseJava/Pizza.java
@@ -0,0 +1,26 @@
+package com.courseJava;
+
+public class Pizza {
+
+    public static String eatPizza(String answer) {
+
+        String answerT = "Yes";
+        String answerF = "No";
+
+        if (answerT.equalsIgnoreCase(answer)) {
+            System.out.printf("%s\n%s%n", "Making pizza!", "Wait 5 seconds...");
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return "Pizza is ready!";
+        } else if (answerF.equalsIgnoreCase(answer)) {
+            return "Bye!";
+        } else {
+            return "Please try again!";
+        }
+    }
+}
